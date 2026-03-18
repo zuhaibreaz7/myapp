05bcf6059e0cb70bab3f1e6e76f0f0dd2a4fcbc9
javeed <108982752+teluguhackerforfree@users.noreply.github.com>
2026-03-18 16:57:35 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,54 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    res.write('Hello from Jenkins + Docker + AWS EC2 Project!');
+
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+
+    res.write(`
+        <html>
+        <head>
+            <title>DevOps Project</title>
+            <style>
+                body {
+                    font-family: Arial, sans-serif;
+                    background: linear-gradient(to right, #4facfe, #00f2fe);
+                    color: white;
+                    text-align: center;
+                    padding-top: 100px;
+                }
+                .card {
+                    background: rgba(0,0,0,0.3);
+                    padding: 30px;
+                    border-radius: 15px;
+                    display: inline-block;
+                }
+                h1 {
+                    font-size: 40px;
+                    margin-bottom: 10px;
+                }
+                p {
+                    font-size: 20px;
+                }
+                .footer {
+                    margin-top: 20px;
+                    font-size: 14px;
+                    color: #ddd;
+                }
+            </style>
+        </head>
+        <body>
+            <div class="card">
+                <h1>🚀 DevOps Project Live</h1>
+                <p>Hello from Jenkins + Docker + AWS EC2 🎉</p>
+                <p>Status: <b style="color:lightgreen;">Running Successfully ✅</b></p>
+                <div class="footer">
+                    <p>Powered by Node.js | Jenkins | Docker | AWS</p>
+                </div>
+            </div>
+        </body>
+        </html>
+    `);
+
     res.end();
 });
 
